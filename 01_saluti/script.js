e6c9be6af9fb22ad35a779eb9a3273ad7f13a7b3
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-17 18:10:42 +0100

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,17 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+function name() {
+    console.log(`Ciao ${userName}`);
+    return
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+name()
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
